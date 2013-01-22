ed8ec5fcb400ddcea37466b642c8a84cc047ee87
EdLeafe <ed@leafe.com>
2013-01-22 17:26:50 -0600

    Reworked to remove the reliance on keyring, which is useless under cron.

--- image_backup/image_backup.py
+++ image_backup/image_backup.py
@@ -3,126 +3,192 @@
 import argparse
 import datetime
 import json
-import keyring
 import logging
 import logging.handlers
+import os
 import sys
 
 import pyrax
 import pyrax.exceptions as exc
 
 
-logger = logging.getLogger("MyLogger")
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.handlers.SysLogHandler()
 logger.addHandler(handler)
+
 
 def logit(msg):
     logger.info("RackspaceImageScheduler: %s" % msg)
 
 
-# Configuration settings are stored using keyring. These
-# are the keys for those settings.
-ID_OF_INSTANCE = "scheduled_image_instance_id"
-ACCOUNT_NAME = "cloud_username"
-COPIES_TO_KEEP = "scheduled_image_number_of_copies"
+# Local file for storing configuration settings.
+CONFIG_FILE = os.path.expanduser("~/.image_backup.cfg")
 
-parser = argparse.ArgumentParser(description="Run regularly scheduled images of a cloud server.")
-parser.add_argument("--username", "-u",  help="The account's username")
-parser.add_argument("--server-id", "-s", action="append", help="The ID of the server to back up. "
-        "You may specify this parameter multiple times to back up multiple servers.")
-parser.add_argument("--retain", "-r", type=int, help="Number of backups to retain")
-parser.add_argument("--persist", "-p", action="store_true", help="Store the values specified in "
-        "this call to be used as the default in future runs.")
-args = parser.parse_args()
 
-username = args.username or keyring.get_password("pyrax", ACCOUNT_NAME)
-instance_ids = args.server_id or keyring.get_password("pyrax", ID_OF_INSTANCE)
-if isinstance(instance_ids, basestring):
-    instance_ids = json.loads(instance_ids)
-num_copies = args.retain or keyring.get_password("pyrax", COPIES_TO_KEEP)
-if args.persist:
-    j_ids = json.dumps(instance_ids)
-    logit("Setting username='%s'; ids='%s'; retention='%s'." % (username, j_ids, num_copies))
-    keyring.set_password("pyrax", ACCOUNT_NAME, username)
-    keyring.set_password("pyrax", ID_OF_INSTANCE, j_ids)
-    keyring.set_password("pyrax", COPIES_TO_KEEP, str(num_copies))
-    
-if not username:
-    username = raw_input("Please enter the account username: ")
-    if not username:
-        logit("Cannot continue without account username.")
-        sys.exit()
-    keyring.set_password("pyrax", ACCOUNT_NAME, username)
-try:
-    pyrax.keyring_auth(username)
-except exc.AuthenticationFailed:
-    api_key = raw_input("Please enter the API key for account '%s': " % username)
-    keyring.set_password("pyrax", username, api_key)
-    try:
-        pyrax.keyring_auth(username)
-    except exc.AuthenticationFailed:
-        logit("Unable to authenticate.")
-        sys.exit()
-# Turn off debug output
-pyrax.set_http_debug(False)
+class SettingsHolder(object):
+    username = None
+    api_key = None
+    instance_ids = None
+    backup_count = None
 
-if not instance_ids:
-    print "Please enter the ID of the server(s) to backup. You may enter more "\
-            "than one ID, separated by spaces. "
-    input_ids = raw_input()
-    if not input_ids:
-        logit("Cannot continue without ID of instance.")
-        sys.exit()
-    # Strip commas, in case they added them to the input
-    input_ids = input_ids.replace(",", "")
-    instance_ids = [iid.strip() for iid in instance_ids.split(" ")]
-    keyring.set_password("pyrax", ID_OF_INSTANCE, json.dumps([instance_ids]))
-
-if not num_copies:
-    num_copies = raw_input("How many backup images should be retained? ")
-    if not num_copies:
-        logit("Cannot continue without copy retention value.")
-        sys.exit()
-    keyring.set_password("pyrax", COPIES_TO_KEEP, num_copies)
-num_copies = int(num_copies)
-
-#TODO: add support for other regions
-cs_dfw = pyrax.connect_to_cloudservers("DFW")
-cs_ord = pyrax.connect_to_cloudservers("ORD")
-regions = (cs_dfw, cs_ord)
-now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-
-for instance_id in instance_ids:
-    for region in regions:
+    def __init__(self, args):
         try:
-            instance = region.servers.get(instance_id)
-            logit("Found instance '%s' in region '%s'" % (instance.name, region.client.region_name))
-            break
-        except exc.ServerNotFound:
-            continue
+            with file(CONFIG_FILE, "rb") as cfile:
+                dct = json.load(cfile)
+        except (IOError, ValueError) as e:
+            logit("Could not read config file: %s" % e)
+            return
+        self.from_dict(dct)
+        # Override with any non-None args
+        for arg in ("username", "api_key", "server_id", "backup_count"):
+            val = getattr(args, arg)
+            if val is not None:
+                if arg == "server_id":
+                    # Naming difference
+                    self.instance_ids = val
+                else:
+                    setattr(self, arg, val)
 
-    image_name = "%s-%s" % (instance.name, now)
+    def save(self):
+        dct = self.to_dict()
+        with file(CONFIG_FILE, "w") as cfile:
+            json.dump(dct, cfile)
 
-    images = region.images.list()
-    backups = [image for image in images
-            if image.name.startswith(instance.name)]
-    # Sorting by name will result in the oldest first.
-    backups.sort(lambda x,y: cmp(x.name, y.name))
+    def from_dict(self, dct):
+        for att in dct:
+            setattr(self, att, dct[att])
 
-    to_delete = []
-    # Don't delete just yet; the image creation step below
-    # could fail.
-    while len(backups) >= num_copies:
-        to_delete.append(backups.pop(0))
+    def to_dict(self):
+        atts = [att for att in self.__dict__ if not att.startswith("_")]
+        ret = {}
+        for att in atts:
+            val = getattr(self, att)
+            if val is not None:
+                ret[att] = val
+        return ret
 
+    def get_missing(self):
+        missing = []
+        if not self.username:
+            missing.append("username")
+        if not self.api_key:
+            missing.append("api_key")
+        if not self.instance_ids:
+            missing.append("instance_ids")
+        if not self.backup_count:
+            missing.append("backup_count")
+        return ", ".join(missing)
+
+
+def main():
     try:
-        instance.create_image(image_name)
-        logit("Successfully created backup image: %s" % image_name)
-    except exc.ServerClientException as e:
-        logit("Unable to create image: %s" % e)
-        continue
+        parser = argparse.ArgumentParser(description="Run regularly scheduled images of a cloud server.")
+        parser.add_argument("--username", "-u",  help="The account's username")
+        parser.add_argument("--api-key", "-k",  help="The account's API key")
+        parser.add_argument("--server-id", "-s", action="append", help="The ID of the server to back up. "
+                "You may specify this parameter multiple times to back up multiple servers.")
+        parser.add_argument("--backup_count", "-b", type=int, help="Number of backups to retain per server.")
+        parser.add_argument("--persist", "-p", action="store_true", help="Store the values specified in "
+                "this call to be used as the default in future runs.")
+        args = parser.parse_args()
 
-    for old_image in to_delete:
-        old_image.delete()
-        logit("Deleted old image: %s" % old_image.name)
+        settings = SettingsHolder(args)
+
+        if isinstance(settings.instance_ids, basestring):
+            settings.instance_ids = json.loads(settings.instance_ids)
+        if args.persist:
+            settings.save()
+
+        # If running under cron, 'interactive' will be False
+        interactive = os.isatty(sys.stdin.fileno())
+
+        if interactive:
+            if not settings.username:
+                settings.username = raw_input("Please enter the account username: ")
+                if not settings.username:
+                    logit("Cannot continue without account username.")
+                    sys.exit()
+            if not settings.api_key:
+                settings.api_key = raw_input("Please enter the account API key: ")
+                if not settings.api_key:
+                    logit("Cannot continue without account API key.")
+                    sys.exit()
+
+            if not settings.instance_ids:
+                print " ".join(["Please enter the ID of the server(s) to backup.",
+                        "You may enter more than one ID, separated by spaces."])
+                input_ids = raw_input()
+                if not input_ids:
+                    logit("Cannot continue without ID of instance.")
+                    sys.exit()
+                # Strip commas, in case they added them to the input
+                input_ids = input_ids.replace(",", "")
+                settings.instance_ids = [iid.strip() for iid in instance_ids.split(" ")]
+
+            if not settings.backup_count:
+                settings.backup_count = raw_input("How many backup images should be retained? ")
+                if not settings.backup_count:
+                    logit("Cannot continue without backup count.")
+                    sys.exit()
+            settings.backup_count = int(settings.backup_count)
+        else:
+            # All values must be defined to continue
+            missing = settings.get_missing()
+            if missing:
+                logit("Could not continue; the following values are missing: %s" % missing)
+                sys.exit()
+
+        try:
+            pyrax.set_credentials(settings.username, settings.api_key)
+        except exc.AuthenticationFailed:
+            logit("Unable to authenticate.")
+            sys.exit()
+        # Turn off debug output
+        pyrax.set_http_debug(False)
+
+        #TODO: add support for other regions
+        cs_dfw = pyrax.connect_to_cloudservers("DFW")
+        cs_ord = pyrax.connect_to_cloudservers("ORD")
+        regions = (cs_dfw, cs_ord)
+        now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+        for instance_id in settings.instance_ids:
+            for region in regions:
+                try:
+                    instance = region.servers.get(instance_id)
+                    logit("Found instance '%s' in region '%s'" % (instance.name, region.client.region_name))
+                    break
+                except exc.ServerNotFound:
+                    continue
+
+            image_name = "%s-%s" % (instance.name, now)
+
+            images = region.images.list()
+            backups = [image for image in images
+                    if image.name.startswith(instance.name)]
+            # Sorting by name will result in the oldest first.
+            backups.sort(lambda x,y: cmp(x.name, y.name))
+
+            to_delete = []
+            # Don't delete just yet; the image creation step below
+            # could fail.
+            while len(backups) >= settings.backup_count:
+                to_delete.append(backups.pop(0))
+
+            try:
+                instance.create_image(image_name)
+                logit("Successfully created backup image: %s" % image_name)
+            except exc.ServerClientException as e:
+                logit("Unable to create image: %s" % e)
+                continue
+
+            for old_image in to_delete:
+                old_image.delete()
+                logit("Deleted old image: %s" % old_image.name)
+    except Exception as e:
+        logit("Exception: %s" % e)
+
+
+if __name__ == "__main__":
+    main()
